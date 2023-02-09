b4e2d744cfd7f2086ec585b10aa8fcbcd69e52e9
markon1i <markoschneider34@gmail.com>
2023-02-09 15:17:16 -0300

    chau api

--- api/src/routes/index.js
+++
@@ -1,8 +0,0 @@
- const { Router } = require("express");
-const propiedades = require("./propiedades.rout");
-const router = Router();
-
-router.use('/propiedades', propiedades);
-
-
-module.exports = router;
